8d626ca1b9aaa27e2bc2a7cdf4c22247aabb78b0
weirdo_kido <263285067+weirdo-kido@users.noreply.github.com>
2026-04-16 00:19:20 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,7 @@
-self.addEventListener('fetch', () => {});
+const C = 'v1';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(C).then(c => c.addAll(['/weirgo/', 'index.html', 'manifest.json', 'favicon.svg'])));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});

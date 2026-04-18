5d6e12a69215fe47f7880de113d19f32380978d1
weirdo_kido <263285067+weirdo-kido@users.noreply.github.com>
2026-04-18 12:16:36 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* 
  * Licensed under the Mozilla Public License, v. 2.0.
- * weirgo SW - Conservative Caching Strategy
+ * weirgo - Conservative Caching Strategy
  */
 
 const C = 'v1';
@@ -13,17 +13,14 @@ const ASSETS = [
 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(C).then(c => c.addAll(ASSETS))
-    .then(() => self.skipWaiting())
+    caches.open(C).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
   );
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys => Promise.all(
-      keys.map(k => {
-        if (k !== C) return caches.delete(k);
-      })
+      keys.filter(k => k !== C).map(k => caches.delete(k))
     )).then(() => self.clients.claim())
   );
 });
@@ -32,17 +29,20 @@ self.addEventListener('fetch', e => {
   if (e.request.method !== 'GET') return;
 
   e.respondWith(
-    caches.match(e.request).then(r => {
-      return r || fetch(e.request).then(res => {
-        if (res.status === 200) {
-          let resClone = res.clone();
-          caches.open(C).then(c => c.put(e.request, resClone));
-        }
-        return res;
-      }).catch(() => {
-        if (e.request.mode === 'navigate') {
-          return caches.match('index.html');
-        }
+    caches.open(C).then(cache => {
+      return cache.match(e.request).then(cachedResponse => {
+
+        const fetchPromise = fetch(e.request).then(networkResponse => {
+          if (networkResponse.ok) {
+            cache.put(e.request, networkResponse.clone());
+          }
+          return networkResponse;
+        }).catch(err => {
+          if (e.request.mode === 'navigate') return caches.match('index.html');
+          throw err;
+        });
+
+        return cachedResponse || fetchPromise;
       });
     })
   );
